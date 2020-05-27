d3893ea77cc51f5c92864c0187f25d52035b6552
Quinn Zilly <quinnz@uw.edu>
2020-05-26 20:50:10 -0700

    Complete aggregate table, Fix summary info

--- scripts/agg_table.R
+++ scripts/agg_table.R
@@ -2,43 +2,41 @@
 
 library(dplyr)
 
-education_df <- read.csv("../data/education_usda.csv", stringsAsFactors = F)
-colnames(education_df)[1] <- gsub('^...', '', colnames(education_df)[1])
+# education_df <- read.csv("../data/education_usda.csv", stringsAsFactors = F)
+# colnames(education_df)[1] <- gsub('^...', '', colnames(education_df)[1])
+# 
+# poverty_df <- read.csv("../data/PovertyEstimates_usda.csv", stringsAsFactors = F)
+# colnames(poverty_df)[1] <- gsub('^...', '', colnames(poverty_df)[1])
+# 
+# unemployment_df <- read.csv("../data/Unemployment_usda.csv", stringsAsFactors = F)
+# colnames(unemployment_df)[1] <- gsub('^...', '', colnames(unemployment_df)[1])
+# 
+# education_poverty_unemployment_df <- education_df %>% 
+#   left_join(poverty_df, by = c("FIPS.Code" = "FIPStxt")) %>% 
+#   left_join(unemployment_df, by = c("FIPS.Code" = "FIPStxt"))
 
-poverty_df <- read.csv("../data/PovertyEstimates_usda.csv", stringsAsFactors = F)
-colnames(poverty_df)[1] <- gsub('^...', '', colnames(poverty_df)[1])
-
-unemployment_df <- read.csv("../data/Unemployment_usda.csv", stringsAsFactors = F)
-colnames(unemployment_df)[1] <- gsub('^...', '', colnames(unemployment_df)[1])
-
-education_poverty_unemployment_df <- education_df %>% 
-  left_join(poverty_df, by = c("FIPS.Code" = "FIPStxt")) %>% 
-  left_join(unemployment_df, by = c("FIPS.Code" = "FIPStxt"))
+# unemployment_kag_df <- read.csv("../data/unemployment_data_us_kaggle.csv",
+#                                 stringsAsFactors = F)
 
 get_agg_table <- function(dataset) {
   agg_table <- dataset %>% 
-    select(State,
-           FIPS.Code,
-           Percent.of.adults.with.less.than.a.high.school.diploma..2014.18,
-           Percent.of.adults.with.a.bachelor.s.degree.or.higher..2014.18,
-           PCTPOVALL_2018,
-           Unemployment_rate_2018) %>% 
-    filter(FIPS.Code %% 1000 != 0) %>%
-    group_by(State) %>% 
-    summarise(mean_county_no_diploma_rate =
-                mean(Percent.of.adults.with.less.than.a.high.school.diploma..2014.18,
-                     na.rm = T),
-              mean_county_bach_rate =
-                mean(Percent.of.adults.with.a.bachelor.s.degree.or.higher..2014.18,
-                     na.rm = T),
-              mean_county_unemployment =
-                mean(Unemployment_rate_2018,
-                     na.rm = T),
-              mean_pov_rate =
-                mean(PCTPOVALL_2018,
-                     na.rm = T))
-  
+    select(Year,
+           Primary_School,
+           High_School,
+           Associates_Degree,
+           Professional_Degree) %>% 
+    filter(Year != 2020) %>%
+    group_by(Year) %>% 
+    summarise(No_Diploma = mean(Primary_School,
+                                                 na.rm = T),
+              Only_High_School = mean(High_School,
+                                                   na.rm = T),
+              Some_College = mean(Associates_Degree,
+                                                   na.rm = T),
+              Bachelors_Degree = mean(Professional_Degree,
+                                                       na.rm = T))
+  colnames(agg_table) <- gsub('_', ' ', colnames(agg_table))
   return(agg_table)
 }
 
-state_aggregate_table <- get_agg_table(education_poverty_unemployment_df)
+#aggregate_table <- get_agg_table(unemployment_kag_df)
--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,6 +1,25 @@
 #summary info
 
 library(dplyr)
+library(readxl)
+
+# education_df <- read_xls("../data/education_usda.xls", range = "Education 1970 to 2018!A5:AU3288")
+# colnames(education_df) <- gsub(' ', '_', colnames(education_df))
+# colnames(education_df) <- gsub(',', '', colnames(education_df))
+# colnames(education_df) <- gsub("'", '', colnames(education_df))
+# colnames(education_df) <- gsub('-', '_', colnames(education_df))
+# 
+# poverty_df <- read_xls("../data/PovertyEstimates_usda.xls", range = "Poverty Data 2018!A5:AB3198")
+# colnames(poverty_df) <- gsub(' ', '_', colnames(poverty_df))
+# colnames(poverty_df) <- gsub(',', '', colnames(poverty_df))
+# 
+# unemployment_df <- read_xls("../data/Unemployment_usda.xls", range = "Unemployment Med HH Income!A8:CJ3283")
+# colnames(unemployment_df) <- gsub(' ', '_', colnames(unemployment_df))
+# colnames(unemployment_df) <- gsub(',', '', colnames(unemployment_df))
+# 
+# education_pov_unemployment_df <- education_df %>%
+#   left_join(poverty_df, by = c("FIPS_Code" = "FIPStxt")) %>%
+#   left_join(unemployment_df, by = c("FIPS_Code" = "FIPStxt"))
 
 #education_df <- read.csv("../data/education_usda.csv", stringsAsFactors = F)
 #colnames(education_df)[1] <- gsub('^...', '', colnames(education_df)[1])
@@ -21,17 +40,17 @@ library(dplyr)
 get_summary_info <- function(dataset) {
   summary_list <- dataset %>% 
     select(State,
-           FIPS.Code,
-           Percent.of.adults.with.less.than.a.high.school.diploma..2014.18,
-           Percent.of.adults.with.a.bachelor.s.degree.or.higher..2014.18,
+           FIPS_Code,
+           Percent_of_adults_with_less_than_a_high_school_diploma_2014_18,
+           Percent_of_adults_with_a_bachelors_degree_or_higher_2014_18,
            PCTPOVALL_2018,
            Unemployment_rate_2018) %>% 
-    filter(FIPS.Code %% 1000 != 0) %>% 
+    filter(as.numeric(FIPS_Code) %% 1000 != 0) %>% 
     summarise(mean_county_no_diploma_rate =
-                mean(Percent.of.adults.with.less.than.a.high.school.diploma..2014.18,
+                mean(Percent_of_adults_with_less_than_a_high_school_diploma_2014_18,
                      na.rm = T),
               mean_county_bach_rate =
-                mean(Percent.of.adults.with.a.bachelor.s.degree.or.higher..2014.18,
+                mean(Percent_of_adults_with_a_bachelors_degree_or_higher_2014_18,
                      na.rm = T),
               mean_county_unemployment =
                 mean(Unemployment_rate_2018,
@@ -50,4 +69,4 @@ get_summary_info <- function(dataset) {
   return(summary_list)
 }
 
-#new_county_summary <- get_summary_info(education_poverty_unemployment_df)
+#new_county_summary <- get_summary_info(education_pov_unemployment_df)
