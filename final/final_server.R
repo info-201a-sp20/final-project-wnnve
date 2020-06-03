b8144f211991d969a706d433d5d76f8fac87f8b6
wnnve@uw.edu <wnnve@uw.edu>
2020-06-03 02:57:06 -0700

    Made changes to overview pages & linted

--- final/final_server.R
+++ final/final_server.R
@@ -8,7 +8,8 @@ library(stringr)
 library(plotly)
 library(readxl)
 
-unemployment_df <- read.csv("../data/unemployment_data_us_kaggle.csv", stringsAsFactors = FALSE)
+unemployment_df <- read.csv("../data/unemployment_data_us_kaggle.csv",
+                            stringsAsFactors = FALSE)
 
 df_1 <- unemployment_df %>%
   group_by(Year) %>%
@@ -36,64 +37,74 @@ new_df <- summary %>%
   filter(Year != "2020")
 
 #Nicole's data
-df <- read.csv("../data/unemployment_data_us_kaggle.csv", stringsAsFactors = FALSE)
+df <- read.csv("../data/unemployment_data_us_kaggle.csv",
+               stringsAsFactors = FALSE)
 df <- mutate(df, avg_unemployment = (White + Black + Asian + Hispanic) / 4)
 
 plot_filter <- df %>%
   filter(Year != "2020") %>%
-  group_by(Month) 
+  group_by(Month)
 
 #Quinn's data
-unemployment_df_usda <- read_xls("../data/Unemployment_usda.xls", range = "Unemployment Med HH Income!A8:CJ3283")
-colnames(unemployment_df_usda) <- gsub(' ', '_', colnames(unemployment_df_usda))
-colnames(unemployment_df_usda) <- gsub(',', '', colnames(unemployment_df_usda))
+unemployment_df_usda <- read_xls("../data/Unemployment_usda.xls",
+                                 range = "Unemployment Med HH Income!A8:CJ3283")
+colnames(unemployment_df_usda) <- gsub(" ", "_", colnames(unemployment_df_usda))
+colnames(unemployment_df_usda) <- gsub(",", "", colnames(unemployment_df_usda))
 
-poverty_df_usda <- read_xls("../data/PovertyEstimates_usda.xls", range = "Poverty Data 2018!A5:AB3198")
-colnames(poverty_df_usda) <- gsub(' ', '_', colnames(poverty_df_usda))
-colnames(poverty_df_usda) <- gsub(',', '', colnames(poverty_df_usda))
+poverty_df_usda <- read_xls("../data/PovertyEstimates_usda.xls",
+                            range = "Poverty Data 2018!A5:AB3198")
+colnames(poverty_df_usda) <- gsub(" ", "_", colnames(poverty_df_usda))
+colnames(poverty_df_usda) <- gsub(",", "", colnames(poverty_df_usda))
 
-poverty_unemployment_state <- poverty_df_usda %>% 
-  left_join(unemployment_df_usda, by = "FIPStxt") %>% 
+poverty_unemployment_state <- poverty_df_usda %>%
+  left_join(unemployment_df_usda, by = "FIPStxt") %>%
   filter(as.numeric(FIPStxt) %% 1000 == 0,
          Stabr.x != "US",
          Stabr.x != "PR",
          Stabr.x != "DC")
 
-education_df_quinn <- unemployment_df %>% 
+education_df_quinn <- unemployment_df %>%
   filter(Month == "Jan",
-         Year != "2020") %>% 
-  select(Year, Primary_School, High_School, Associates_Degree, Professional_Degree)
+         Year != "2020") %>%
+  select(Year, Primary_School, High_School,
+         Associates_Degree, Professional_Degree)
 
-#SERVER 
+#SERVER
 server <- function(input, output) {
 
   output$plot <- renderPlotly({
-    
+
     # ggplot bar
     title <- paste0("Average Unemployment Rate by Education Level Dataset")
-    
+
     plot <- ggplot(data = new_df,
-                   mapping = aes(x = Year, y = avg_unemployment_rate, text = paste("Year: ", Year))) +
+                   mapping = aes(x = Year, y = avg_unemployment_rate,
+                                 text = paste("Year: ", Year))) +
       geom_col(mapping = aes(fill = education_level), stat = "identity") +
       theme_classic() +
       labs(title = title,
            x = "Year",
            y = "Unemployment Rate",
            fill = "Education Level") +
-      scale_x_continuous(breaks = seq(2010, 2019, 1), limits = range(input$year_var)) +
+      scale_x_continuous(breaks = seq(2010, 2019, 1),
+                         limits = range(input$year_var)) +
       scale_y_continuous(labels = function(y) paste0(y, "%"))
     return(plot)
   })
- 
-  #Nicole's Page 
-  #Graph #1
+
+#Nicole's Page
+#Graph #1
   output$demographic_plotly <- renderPlot({
     plot <-   ggplot(data = plot_filter) +
       geom_line(mapping = aes(x = reorder(Month, -avg_unemployment),
-                              y = avg_unemployment, color = "AVERAGE RATE", group = 1)) +
-      geom_line(mapping = aes(x = Month, y = White, color = "WHITE", group = 1)) +
-      geom_line(mapping = aes(x = Month, y = Asian, color = "ASIAN", group = 1)) +
-      geom_line(mapping = aes(x = Month, y = Black, color = "BLACK", group = 1)) +
+                              y = avg_unemployment,
+                              color = "AVERAGE RATE", group = 1)) +
+      geom_line(mapping = aes(x = Month, y = White,
+                              color = "WHITE", group = 1)) +
+      geom_line(mapping = aes(x = Month, y = Asian,
+                              color = "ASIAN", group = 1)) +
+      geom_line(mapping = aes(x = Month, y = Black,
+                              color = "BLACK", group = 1)) +
       geom_line(mapping = aes(x = Month,
                               y = Hispanic, color = "HISPANIC", group = 1)) +
       facet_wrap(~Year) +
@@ -106,17 +117,17 @@ server <- function(input, output) {
       )
     return(plot)
   })
-  
-#Graph #2  
+
+#Graph #2
   output$select_plotly <- renderPlotly({
     plot_filter2 <- plot_filter %>%
       filter(Year == input$selectyear) %>%
       group_by(Month)
-    plot_filter2$month_num <- 1:12 
-    plot_filter2 %>%   
+    plot_filter2$month_num <- 1:12
+    plot_filter2 %>%
       arrange(month_num)
-    
-    plot3 <- 
+
+    plot3 <-
       plot_ly(data = plot_filter2) %>%
       add_trace(
         x = ~month_num,
@@ -170,59 +181,64 @@ server <- function(input, output) {
       ) %>%
       layout(
         title = "Unemployment Rate Among Race",
-        yaxis = list(tickformat = "", 
+        yaxis = list(tickformat = "",
                      title = "Unemployment Rate (%)"),
         xaxis = list(tickformat = "",
                      title = "Month")
       )
-    
+
     return(plot3)
-  }) 
-  
-  #Winnie's page
-  
+  })
+
+#Winnie's page
+
   #A visualization function
   output$interactive_graph <- renderPlotly({
     visualization <- function(dataset, region) {
       #GGPLOT
       plot <- ggplot(data = dataset) +
-        geom_point(mapping = aes(x = unemployment_rate, y = people_in_poverty, 
-                                 text = paste("State: ", State))) + theme_classic() +
+        geom_point(mapping = aes(x = unemployment_rate, y = people_in_poverty,
+                                 text = paste("State: ", State))) +
+        theme_classic() +
         labs(
-          title = paste("Unemployment vs Number of People in Poverty By", region),
+          title = paste("Unemployment vs Number of People in Poverty By",
+                        region),
           x = "Unemployment Rate", y = "People in Poverty (in Millions)")
       return(plot)
     }
-    
+
     #Reads in CSV file
-    unemployment_vs_poverty <- 
+    unemployment_vs_poverty <-
       read.csv("../data/Unemployement_Poverty_2016_kaggle.csv",
                stringsAsFactors = FALSE)
-    
+
     #Filters the dataframe into a small dataframe by state
     unemployment_by_state <- unemployment_vs_poverty %>%
       group_by(State) %>%
       summarise(
         unemployment_rate = sum(Unemployment_rate_2016, na.rm = TRUE) / 10,
         people_in_poverty = sum(POVALL_2016, na.rm = TRUE) / 1000) %>%
-      filter(unemployment_rate > input$slider2[1] & unemployment_rate < input$slider2[2]) %>%
+      filter(unemployment_rate > input$slider2[1] &
+             unemployment_rate < input$slider2[2]) %>%
       arrange(desc(unemployment_rate))
-    
+
     visualization(unemployment_by_state, "State")
   })
   #The slider used to adjust the unemployment rate
-  output$range <- renderPrint({input$slider2})
-  
-  #Quinn's Page
-  
+  output$range <- renderPrint({
+                    input$slider2
+                  })
+
+#Quinn's Page
+
   #Race Plot
   output$quinn_race <- renderPlot({
     #data manipulation
-    race_df <- unemployment_df %>% 
-      select(Year, Month, White, Black) %>% 
+    race_df <- unemployment_df %>%
+      select(Year, Month, White, Black) %>%
       filter(Month == "Jan",
              Year != "2020")
-    
+
     #Line/Scatter Plot
     g <- ggplot(data = race_df)
     g + geom_line(mapping = aes(x = Year,
@@ -239,36 +255,34 @@ server <- function(input, output) {
            y = "Unemployment Rate",
            color = "Race")
   })
-  
+
   #Education Table
   output$quinn_education <- renderDataTable(
     education_df_quinn,
     options = list(searching = FALSE,
                    scrollX = TRUE)
   )
-  
+
   #Poverty Plot
   output$quinn_poverty_map <- renderPlotly({
-    plot_ly (
-      type = "choropleth" ,
-      locations = poverty_unemployment_state$Stabr.x ,
-      locationmode = "USA-states" ,
-      colorscale = "Viridis" ,
+    plot_ly(
+      type = "choropleth",
+      locations = poverty_unemployment_state$Stabr.x,
+      locationmode = "USA-states",
+      colorscale = "Viridis",
       z = poverty_unemployment_state$PCTPOVALL_2018) %>%
-      colorbar(title = "Poverty Rate") %>% 
-      layout ( geo = list ( scope = "usa" ))
+      colorbar(title = "Poverty Rate") %>%
+      layout(geo = list(scope = "usa"))
   })
-  
+
   output$quinn_unemployment_map <- renderPlotly({
-    plot_ly (
-      type = "choropleth" ,
-      locations = poverty_unemployment_state$Stabr.x ,
-      locationmode = "USA-states" ,
-      colorscale = "Hot" ,
+    plot_ly(
+      type = "choropleth",
+      locations = poverty_unemployment_state$Stabr.x,
+      locationmode = "USA-states",
+      colorscale = "Hot",
       z = poverty_unemployment_state$Unemployment_rate_2018) %>%
-      colorbar(title = "Unemployment Rate") %>% 
-      layout ( geo = list ( scope = "usa" ))
+      colorbar(title = "Unemployment Rate") %>%
+      layout(geo = list(scope = "usa"))
   })
 }
-
-
--- final/final_ui.R
+++ final/final_ui.R
@@ -15,54 +15,63 @@ main_content <- mainPanel(
 
 education_panel <- tabPanel(
   strong("Unemployment vs Education Chart"),
-  titlePanel("Unemployment vs Education"),
+  titlePanel(strong("Unemployment vs Education")),
   sidebarLayout(
     year_content,
     main_content
   ),
-  h2(strong("Findings")),
+  h2("Findings"),
   p("I included this stacked bar chart because it compares
-    the unemployment rates of each education level by year. 
-    This is easier to see the differences. The chart reveals 
-    the average unemployment rate for each education level by year. 
-    One thing I observed from this was that the discrepancy between 
-    a primary school education level and a professional school 
+    the unemployment rates of each education level by year.
+    This is easier to see the differences. The chart reveals
+    the average unemployment rate for each education level by year.
+    One thing I observed from this was that the discrepancy between
+    a primary school education level and a professional school
     education level got smaller as the years passed. In addition,
-    the chart descended as the years passed, which means that 
+    the chart descended as the years passed, which means that
     unemployment levels decreased.")
 )
 
 overview_panel <- tabPanel(
   strong("Overview"),
   titlePanel("Unemployment Overview"),
-  p("Unemployment has been at an all time high because of the coronavirus, 
-  however, we want to analyze what other factors could affect unemployment. We specifically
-    analyzed poverty, education, and demographics. Now with our findings, we decided to exclude
-    the year 2020 because we did not have the sufficient amount of information to analyze our data.
-    The numbers would have been skewed and incorrect, thus impacting the rest of our data analysis."),
-  h2("Questions:"),
-  p("Some questions we are seeking to answer are how is unemployment correlated with poverty and 
-    education? Are there demographic patterns surrounding unemployment? We will be using ____ and ___ 
-    datasets to answer these questions. The first dataset we looked at was unemployment vs poverty. 
-    This dataset answers questions of how unemployment is spread throughout the U.S. 
-    and we can also compare that to how poverty is spread throughout the U.S. 
-    This will allow us to examine the relationship between unemployment and poverty 
-    and see what states and counties have the highest prevalence.
-    The second dataset we looked at answers how education correlates with unemployment. 
-    It contains the unemployment rate based on the education qualification of adults that went
-    to primary school, high school, Associates, and Professional degree. It also has the unemployment
-    rate based on the race of adults."),
-  sidebarLayout(
-    img(src = "https://media.heartlandtv.com/images/Unemployment+image+4.jpg",
-        width = "900",
+  sidebarPanel(
+    h2("Questions:"),
+    p("Some questions we are seeking to answer are:"),
+    p("How is unemployment correlated with poverty and education?"),
+    p("Are there demographic patterns surrounding unemployment?")),
+  mainPanel(
+    img(src = "https://media.brstatic.com/2020/04/07164233/how-nations-unemployment-insurance-system-works.jpeg",
+        width = "700",
         height = "400"),
-    p("Unemployment has always been prevelent.... ")
+    h2("Overview"),
+    p("Unemployment has been at an all time high because of the coronavirus;
+    however, we want to analyze what other factors that could affect
+    unemployment. We specifically analyzed poverty, education, demographics.
+    With our findings, we decided to exclude the year 2020
+    because it did not have the sufficient amount of information to
+    analyze our data. The numbers would have been skewed and incorrect,
+    thus impacting the rest of our data analysis."),
+    h2("Summary of the Datasets"),
+    p("The first dataset we looked at was unemployment vs poverty.
+      This dataset answers questions of how unemployment is spread throughout
+      the U.S. and we can also compare that to how poverty is spread
+      throughout the U.S. This will allow us to examine the relationship
+      between unemployment and poverty and see what states and counties
+      have the highest prevalence."),
+    p("The second dataset we looked at answers to how education correlates
+      with unemployment. It contains the unemployment rate based on the
+      education qualification of adults that went to primary, middle,
+      and high school. As well as those that received an Associates
+      and/or Professional degree. It also has the unemployment rate
+      based on the race of adults."),
+    #p("Unemployment has always been prevelent.... ")
   )
 )
 
 
 
-#Nicole's code 
+#Nicole's code
 
 year_selected <- sidebarPanel(
   h2("Choose which year to view:"),
@@ -71,13 +80,13 @@ year_selected <- sidebarPanel(
     label = h3("Year"),
     choices = list("2010" = 2010,
                    "2011" = 2011,
-                   "2012" = 2012, 
+                   "2012" = 2012,
                    "2013" = 2013,
                    "2014" = 2014,
                    "2015" = 2015,
-                   "2016" = 2016, 
-                   "2017" = 2017, 
-                   "2018" = 2018, 
+                   "2016" = 2016,
+                   "2017" = 2017,
+                   "2018" = 2018,
                    "2019" = 2019)
   )
 )
@@ -95,24 +104,24 @@ main_graph <- mainPanel(
 )
 
 demographic_panel <- tabPanel(
-  strong("Demographics"),
-  titlePanel("Unemployment Among Race"),
+  strong("Demographics Chart"),
+  titlePanel(strong("Unemployment Among Race")),
   sidebarLayout(
     year_selected,
     year_graph
   ),
   main_graph,
-  h2(strong("Findings")),
-  p("These charts reveal that among black, white, hispanic, 
-    and asian people, black people had a higher unemployment 
-    rate from 2010 - 2019. The green/yellow line shows the average 
-    unemployment rate and this allows us to see that asian 
-    and white people are below the average unemployment rate 
-    while hispanic people are close to average and black people 
-    are above average. This graph creates a visualization to see 
-    how unemployment rates were widespread in 2010, but overtime, have 
-    decreased and have become slightly more uniform. However, black 
-    people still have the highest rate of unemployment and 
+  h2("Findings"),
+  p("These charts reveal that among black, white, hispanic,
+    and asian people, black people had a higher unemployment
+    rate from 2010 - 2019. The green/yellow line shows the average
+    unemployment rate and this allows us to see that asian
+    and white people are below the average unemployment rate
+    while hispanic people are close to average and black people
+    are above average. This graph creates a visualization to see
+    how unemployment rates were widespread in 2010, but overtime, have
+    decreased and have become slightly more uniform. However, black
+    people still have the highest rate of unemployment and
     disparities are still present.")
 )
 
@@ -124,34 +133,40 @@ page_one <- mainPanel(
 )
 
 page_one_sliderbar <- sidebarPanel(
-  sliderInput("slider2", label = h3("Adjust Unemployment Rate"), min = 0, 
+  sliderInput("slider2", label = h3("Adjust Unemployment Rate"), min = 0,
               max = 150, value = c(0, 150)),
   p("You can adjust the range of the unemployment rate by sliding the slider")
 )
 
 unemployment_panel <- tabPanel(
-  strong("Unemployment vs Poverty"),
-  titlePanel("How Unemployment Effects Poverty By States"),
+  strong("Unemployment vs Poverty Chart"),
+  titlePanel(strong("The Effects of Unemployment on Poverty by States")),
   sidebarLayout(
     page_one,
     page_one_sliderbar
   ),
   h2("Findings"),
-  p("This graph shows the unemployment rate and the people in poverty by state. 
-    When you hover over the dots, it'll show the state, the number of people in poverty,
-    and the unemployment rate for that state. This chart reveals that unemployment and the 
-    people in poverty are mostly correlated. As you can see, one of the furthest dot, Georgia,
-    has a high unemployment rate but the number of people in poverty isn't as high as other
-    states that have a higher unemployment rate. Besides a few outliers, the unemployment
-    rate usually corresponds to the number of people in poverty. ")
+  p("This graph shows the unemployment rate and the people in poverty by state.
+    This chart reveals that there's a positive correlation between
+    unemployment and people in poverty. The lower the unemployment,
+    the lower the number of people in poverty. It also shows that many
+    states don't have high unemployment and people in poverty;
+    however, there are no states that has exactly zero.
+    In addition, we see a few outliers in the chart, two specifically
+    that are way out of line with the rest of the states.
+    As you can see, one of the furthest dot, Georgia,
+    has a high unemployment rate but the number of people in poverty
+    isn't as high as other states that have a higher unemployment rate.
+    Regardless of the few outliers, we can see that there is a positive
+    correlation between unemployment and people in poverty.")
   )
 
 #Quinn's Page
 
-title_content_quinn <- titlePanel("Summary Page")
+title_content_quinn <- titlePanel(strong("Summary Page"))
 
-intro_row <- fluidRow(column(width = 12,
-                             p("Intro")))
+#intro_row <- fluidRow(column(width = 12,
+#                            p("Intro")))
 
 ed_row <- fluidRow(column(width = 6,
                           h2("Effect of Education on Unemployment"),
@@ -206,13 +221,29 @@ summary_panel <- tabPanel(
   strong("Summary"),
   fluidPage(
     title_content_quinn,
-    intro_row,
+    #intro_row,
     ed_row,
     race_row,
     poverty_row
   )
 )
 
+source_panel <- tabPanel(
+  strong("Sources"),
+  h2(a("Unemployment Poverty 2016",
+       href = "https://www.kaggle.com/ravidawar/unemployment-poverty-2016")),
+  p("This data set has unemployment rates from the Bureau of Labor Statistics
+    (BLS) and Local Area Unemployment Statistics (LAUS) program. It also has
+    data from the Small Area Income and Poverty Estimate (SAIPE) program.
+    The data compares unemployment and poverty rate across states
+    and counties."),
+  h2(a("US Unemployment Dataset",
+       href = "https://www.kaggle.com/aniruddhasshirahatti/us-unemployment-dataset-2010-2020")),
+  p("The datasets contains a time series data of unemployment rate
+    in the US from Jan. 2010 to present. It contains unemployment
+    rate records based on education qualification, race,
+    and gender of adults. It also contains state-wise unemployment")
+)
 ui <- navbarPage(
   theme = shinytheme("flatly"),
 
@@ -221,5 +252,6 @@ ui <- navbarPage(
   education_panel,
   demographic_panel,
   unemployment_panel,
-  summary_panel
+  summary_panel,
+  source_panel
 )
